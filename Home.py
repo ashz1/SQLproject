331def275458b2c1f86b84a8e557a10f7897187f
zendeaashay <148306495+zendeaashay@users.noreply.github.com>
2024-07-07 08:28:54 +0930

    nm

--- Home.py
+++ Home.py
@@ -7,10 +7,23 @@ fdf = pd.read_csv('data/1.csv')
 adf = pd.read_csv('data/2.csv')
 conn = sqlite3.connect('ecom.db')
 st.title("SQL Database Operations Demo by Aashay")
-st.write("""Introduction:
+container.write("""Introduction:
 I've created a simple CRUD SQL demo app using SQLite3 to demonstrate SQL queries. This data is based on my experience as a business and data analyst in India, although I've altered it significantly for demonstration purposes.
 
 App Overview:
 The app, built using Python's Streamlit library, allows users to perform basic CRUD (Create, Read, Update, Delete) operations on two datasets: one from Flipkart and one from Amazon. It provides an interactive interface for viewing, searching, updating, and deleting data, along with the SQL queries used. 
 The code can be viewed on github, under the MIT license, feel free to use it for your own projects and please do not hesitate to drop me an email if you find any errors or have feedback, I'll appreciate it.
 """)
+# Add a new column to identify the source
+fdf['Source'] = 'Flipkart'
+adf['Source'] = 'Amazon'
+# Reorder columns to place 'Source' after 'Month'
+cols = fdf.columns.tolist()
+cols.insert(cols.index('Month') + 1, cols.pop(cols.index('Source')))
+fdf = fdf[cols]
+adf = adf[cols]
+# Function to create database and insert data from CSV
+def create_database():
+    fdf.to_sql('flipkart', conn, if_exists="replace", index=False)
+    adf.to_sql('amazon', conn, if_exists="replace", index=False)
+    conn.commit()
