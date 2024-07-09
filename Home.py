e5439fb041187591e81b7cf50655bab1551acf61
zendeaashay <148306495+zendeaashay@users.noreply.github.com>
2024-07-09 19:29:22 +0930

    rwg

--- Home.py
+++ Home.py
@@ -1,82 +1,16 @@
-import sqlite3
 import streamlit as st
-import pandas as pd
-container = st.container(border=True)
-container1 = st.container(border=True)
-fdf = pd.read_csv('data/1.csv')
-adf = pd.read_csv('data/2.csv')
-conn = sqlite3.connect('ecom.db')
+
+# Container for title and introduction
+container = st.container()
+container1 = st.container()
+
 container.write("SQL Database Operations Demo by Aashay")
-container1.write("""Introduction:
+container1.write("""
+# Introduction:
 I've created a simple CRUD SQL demo app using SQLite3 to demonstrate SQL queries. This data is based on my experience as a business and data analyst in India, although I've altered it significantly for demonstration purposes.
 
-App Overview:
+## App Overview:
 The app, built using Python's Streamlit library, allows users to perform basic CRUD (Create, Read, Update, Delete) operations on two datasets: one from Flipkart and one from Amazon. It provides an interactive interface for viewing, searching, updating, and deleting data, along with the SQL queries used. 
-The code can be viewed on github, under the MIT license, feel free to use it for your own projects and please do not hesitate to drop me an email if you find any errors or have feedback, I'll appreciate it.
+
+The code can be viewed on GitHub, under the MIT license. Feel free to use it for your own projects, and please do not hesitate to drop me an email if you find any errors or have feedback. I'll appreciate it.
 """)
-# Add a new column to identify the source
-fdf['Source'] = 'Flipkart'
-adf['Source'] = 'Amazon'
-# Reorder columns to place 'Source' after 'Month'
-cols = fdf.columns.tolist()
-cols.insert(cols.index('Month') + 1, cols.pop(cols.index('Source')))
-fdf = fdf[cols]
-adf = adf[cols]
-# Function to create database and insert data from CSV
-def create_database():
-    fdf.to_sql('flipkart', conn, if_exists="replace", index=False)
-    adf.to_sql('amazon', conn, if_exists="replace", index=False)
-    conn.commit()
-
-# Function to view all data in a table
-def view_data(table):
-    if table == 'both':
-        query_flipkart = f"SELECT * FROM flipkart"
-        query_amazon = f"SELECT * FROM amazon"
-        result_flipkart = pd.read_sql(query_flipkart, conn)
-        result_amazon = pd.read_sql(query_amazon, conn)
-        return pd.concat([result_flipkart, result_amazon])
-    else:
-        query = f"SELECT * FROM {table}"
-        return pd.read_sql(query, conn)
-
-def main():
-    
-
-    # Create database and insert data from CSV if not already created
-    create_database()
-
-    # View operations
-    st.sidebar.header("View Operations: 'SELECT * FROM {table}'")
-    
-    table_to_view = st.sidebar.selectbox("Choose a table to view", ["flipkart", "amazon", "both"], key="view_table")
-    
-
-
-    if st.sidebar.button("Click here to view"):
-        result = view_data(table_to_view)
-        st.header(f"Data in {table_to_view} Table:")
- 
-        st.write("""
-            SQL CREATE VIEW Statement
-             
-                 In SQL, a view is a virtual table based on the result-set of an SQL statement.
-                A view contains rows and columns, just like a real table. The fields in a view are fields from one or more real tables in the database.
-                You can add SQL statements and functions to a view and present the data as if the data were coming from one single table. """)
-        st.write("""
-            A view is created with the CREATE VIEW statement. 
-                
-                 CREATE VIEW Syntax
-                CREATE VIEW view_name AS
-                SELECT column1, column2, ...
-                FROM table_name
-                WHERE condition; """)
-        st.write(""" 
-                 
-                 Note: A view always shows up-to-date data! The database engine recreates the view, every time a user queries it.""")
-        st.write(result)
-
-        conn.close()
-    
-if __name__ == '__main__':
-    main()
---
+++ pages/View Operation.py
@@ -0,0 +1,74 @@
+import sqlite3
+import streamlit as st
+import pandas as pd
+
+# Read CSV files
+fdf = pd.read_csv('data/1.csv')
+adf = pd.read_csv('data/2.csv')
+
+# Add a new column to identify the source
+fdf['Source'] = 'Flipkart'
+adf['Source'] = 'Amazon'
+
+# Reorder columns to place 'Source' after 'Month'
+cols = fdf.columns.tolist()
+cols.insert(cols.index('Month') + 1, cols.pop(cols.index('Source')))
+fdf = fdf[cols]
+adf = adf[cols]
+
+# Connect to SQLite database
+conn = sqlite3.connect('ecom.db')
+
+# Function to create database and insert data from CSV
+def create_database():
+    fdf.to_sql('flipkart', conn, if_exists="replace", index=False)
+    adf.to_sql('amazon', conn, if_exists="replace", index=False)
+    conn.commit()
+
+# Function to view all data in a table
+def view_data(table):
+    if table == 'both':
+        query_flipkart = "SELECT * FROM flipkart"
+        query_amazon = "SELECT * FROM amazon"
+        result_flipkart = pd.read_sql(query_flipkart, conn)
+        result_amazon = pd.read_sql(query_amazon, conn)
+        return pd.concat([result_flipkart, result_amazon])
+    else:
+        query = f"SELECT * FROM {table}"
+        return pd.read_sql(query, conn)
+
+def main():
+    # Create database and insert data from CSV if not already created
+    create_database()
+
+    # View operations
+    st.sidebar.header("View Operations: 'SELECT * FROM {table}'")
+    table_to_view = st.sidebar.selectbox("Choose a table to view", ["flipkart", "amazon", "both"], key="view_table")
+
+    if st.sidebar.button("Click here to view"):
+        result = view_data(table_to_view)
+        st.header(f"Data in {table_to_view.capitalize()} Table:")
+
+        st.write("""
+            ## SQL CREATE VIEW Statement
+             
+            In SQL, a view is a virtual table based on the result-set of an SQL statement.
+            A view contains rows and columns, just like a real table. The fields in a view are fields from one or more real tables in the database.
+            You can add SQL statements and functions to a view and present the data as if the data were coming from one single table.
+            
+            ### CREATE VIEW Syntax
+            ```
+            CREATE VIEW view_name AS
+            SELECT column1, column2, ...
+            FROM table_name
+            WHERE condition;
+            ```
+
+            Note: A view always shows up-to-date data! The database engine recreates the view every time a user queries it.
+        """)
+        st.write(result)
+
+        conn.close()
+
+if __name__ == '__main__':
+    main()
